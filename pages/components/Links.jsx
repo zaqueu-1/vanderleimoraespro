14c1ede428db53db1644948f8a54c8b8771f1c1e
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-05-18 09:34:19 -0300

    new cards

--- pages/components/Links.jsx
+++ pages/components/Links.jsx
@@ -5,13 +5,17 @@ import { CgGym } from 'react-icons/cg'
 function Links() {
   return (
     <div className='links'>
-        <a href="https://wa.me/24981236824" className='link' target="_blank" rel="noreferrer">
-            <CgGym className='small-icon' />
-            <span>FAÇA PARTE DO TIME | CONSULTORIA</span>
+        <a href="https://wa.me/24981236824" className='link l1' target="_blank" rel="noreferrer">
+            <div className="link-desc">
+              <CgGym className='small-icon' />
+              <span>FAÇA PARTE DO TIME | CONSULTORIA</span>
+            </div>
         </a>
-        <a href="https://www.youtube.com/channel/UCEz26k3gcRbSirXZlVg7KuA" className='link' target="_blank" rel="noreferrer">
-            <BsYoutube className='small-icon' /> 
-            <span>ACESSE MEU CONTEÚDO | YOUTUBE</span>
+        <a href="https://www.youtube.com/channel/UCEz26k3gcRbSirXZlVg7KuA" className='link l2' target="_blank" rel="noreferrer">
+            <div className="link-desc">
+              <BsYoutube className='small-icon' /> 
+              <span>ACESSE MEU CONTEÚDO | YOUTUBE</span>
+            </div>
         </a>
     </div>
   )
